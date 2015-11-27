01b3edbd2ea9205de9b3867eac6b2a6af08aaf56
sercco <sergibdn94@gmail.com>
2015-11-27 12:00:18 +0100

    el k te focka

---
+++ runner3D/Assets/scripts/groundDetection.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var onGround : boolean;
+
+function Start () {
+	onGround = false;
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter()
+{
+	onGround = true;
+}
+
+function OnTriggerExit()
+ {
+ 	onGround = false;
+ }
